f6eb536e97aae223d423ffb4400a62e096208c83
Eric Denis Laura <ericdenislaura@gmail.com>
2023-06-06 02:48:45 -0300

    create main with snipped mate

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
+      title: 'TokTik',
       debugShowCheckedModeBanner: false,
       theme: AppTheme().getTheme(),
       home: Scaffold(
